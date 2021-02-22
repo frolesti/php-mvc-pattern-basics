8232e49bec1e427c21a407544fc60fc1e6a9ad23
wsierra010 <wsierra010@gmail.com>
2021-02-22 16:19:11 +0100

    GetById & Get function

--- config/constants.php
+++ config/constants.php
@@ -1,13 +1,15 @@
 <?php
 
+$BASE_PATH = $_SERVER['SCRIPT_FILENAME'];
+
 //CONTROLLERS
-define("CONTROLLERS", BASE_PATH . '/controllers/');
+define("CONTROLLERS", $BASE_PATH . '/controllers/');
 
 //VIEWS
-define("VIEWS", BASE_PATH . '/views/');
+define("VIEWS", $BASE_PATH . '/views/');
 
 //MODELS
-define("MODELS", BASE_PATH . '/models/');
+define("MODELS", $BASE_PATH . '/models/');
 
 //RESOURCES
-define("RESOURCES", BASE_PATH . '/resources/');
+define("RESOURCES", $BASE_PATH . '/resources/');
