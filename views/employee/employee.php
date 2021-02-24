b6a0c37c9029cba6ae2a8e6db88cdb1acfc22fdc
Froilán <frolesti4@gmail.com>
2021-02-24 10:11:41 +0100

    css finished

--- views/employee/employee.php
+++ views/employee/employee.php
@@ -4,31 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/dashboard.css">
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&display=swap" rel="stylesheet">
     <title>Employees MySQL</title>
 </head>
 <body>
 
-    <table>
-
-        <thead>
-            <tr>
-                <td>ID</td>
-                <td>Employee</td>
-                <td>Gender</td>
-            </tr>
-        </thead>
-
-        <tbody>
+    
             <?php
-                    echo "<tr>";
-                    echo "<td>". $employee[0]['emp_no'] ."</td>";
-                    echo "<td>". $employee[0]['first_name'] . ' ' . $employee[0]["last_name"] . "</td>";
-                    echo "<td>". $employee[0]['gender'] ."</td>";
-                    echo "</tr>";
-
+                if($employee[0]["gender"] == "M"){
+                    $employee[0]["gender"] = "Male";
+                } else {
+                    $employee[0]["gender"] = "Female";
+                }
+                $employeeName = $employee[0]["first_name"] . ' ' . $employee[0]["last_name"];
+                echo '<div class="employeeCard">
+                        <section class="employeeImageContainer">
+                            <img src="assets/img/employee.png" alt="">
+                        </section>
+                        <section class="employeeName">'. $employeeName . '</section>
+                        <section class="employeeInfo">
+                            Gender: ' . $employee[0]["gender"].'<br> 
+                            Hire Date: '. $employee[0]["hire_date"] .'<br>
+                            Birth Date: '. $employee[0]["birth_date"] .'
+                        </section>
+                    </div>';
             ?>
-        </tbody>
-
-    </table>
-
 </body>
